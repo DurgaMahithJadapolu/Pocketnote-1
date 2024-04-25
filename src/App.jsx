b15a1e44a0b51f9137538bf787de3970b2ffe7d1
DurgaMahithJadapolu <158590615+DurgaMahithJadapolu@users.noreply.github.com>
2024-04-25 14:47:12 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+// import DmainPg from './Page/Main-page/DmainPg'
+// import LeftBody from './components/LeftBody'
+import  Rightbody from './components/RightDetails'
+
+function App() {
+    
+ return(<>
+ <Rightbody/>
+ {/* <LeftBody/> */}
+     {/* <DmainPg/> */}
+ </>)
+}
+
+export default App
